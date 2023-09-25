de850e4212b2a1db348039c0186d55145d83e7eb
Juan Pablo Tobar <juanpablococtm@gmail.com>
2023-09-25 07:19:48 -0500

    Inventario de productos

---
+++ app_inventario.java
@@ -0,0 +1,23 @@
+import javax.swing.JOptionPane;
+
+public class app_inventario {
+
+  static cls_productos[] productos  = new cls_productos[100];
+  static int int_posicionesP = 0;
+
+  public static void main(String[] args){
+    fnt_menuP(true);
+  }
+  private static void fnt_menuP(boolean men){
+    while(men == true) {
+      int m = Integer.parseInt(JOptionPane.showInputDialog(null, 
+      "MENU PRINCIPAL\n" + 
+      "1. Registrar producto\n " +
+      "2. Consultar producto\n " +
+      "3. Acutalizar producto\n " +
+      "4. Registro de compras\n " +
+      "5. Reportes " +
+      "6. Salir\n "));
+    }
+  }
+}
